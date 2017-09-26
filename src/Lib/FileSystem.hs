dcdb4167f3111281da90ee15a02cd929fb1bae9c
Brian Chan <i@b123400.net>
2017-09-26 01:53:38 -0400

    Recursively create directory

--- src/Lib/FileSystem.hs
+++ src/Lib/FileSystem.hs
@@ -1,11 +1,20 @@
 module Lib.FileSystem where
 
+import Data.Maybe (Maybe(..), maybe)
 import System.Directory (doesDirectoryExist, createDirectory)
+import System.FilePath.Posix (joinPath, splitPath)
 
 prepareDirectory :: String -> IO ()
 prepareDirectory path =
-  doesDirectoryExist path >>= \exist->
-  if exist
-    then return ()
-    else createDirectory path
+  maybe (return ()) prepareDirectory (parent path)
+  >> doesDirectoryExist path
+  >>= \exist-> if exist
+               then return ()
+               else createDirectory path
 
+parent :: String -> Maybe String
+parent path =
+  case splitPath path of
+    []      -> Nothing
+    [_]     -> Nothing -- Root layer should be ignored
+    folders -> Just $ joinPath $ init folders
